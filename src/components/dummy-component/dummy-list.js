2eeb8ecfff585f40232b74042b1a8792284e9a21
Bala <sbs3895@gmail.com>
2020-10-03 22:04:40 +0530

    Dummy List Component

---
+++ src/components/dummy-component/dummy-list.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+export const BalaComp = function(){
+    return <div>BALA</div>;
+}
+class Dummyhome extends Component{
+    render(){
+        return (<div>
+            <h1>This is Dummyhome</h1>
+            <button>Click</button>
+        </div>);
+    }
+}
+
+export default Dummyhome;
